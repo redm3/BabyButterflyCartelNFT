db2c7534bed4b9e7fa1dbafbbfe49e2396150a3b
marcowells10 <56494159+marcowells10@users.noreply.github.com>
2022-11-21 20:06:41 +1300

    Update IREWARDSDISTRIBUTOR.sol

--- IREWARDSDISTRIBUTOR.sol
+++ IREWARDSDISTRIBUTOR.sol
@@ -1,19 +1,19 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-//@param  claims  Claim[] List of claim metadata
-//claims (tuple[])
+library Common {
     struct Claim {
         address token;
         address account;
         uint256 amount;
         bytes32[] merkleProof;
     }
+}
 
-    interface IREWARDSDISTRIBUTOR {
-        function claim(Claim[] calldata claims) external;
-        }
+    interface IRewardDistributor {
+            function claim(Common.Claim[] calldata claims) external;
 
+    }
 
 
 
